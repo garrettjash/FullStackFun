2062eb5e5ed6004e769aad5f1abf66092bce0a96
garrettjash <garrettashcroft@gmail.com>
2024-03-12 14:56:26 -0600

    Final

---
+++ backend/APIFun/Controllers/MarriottFoodController.cs
@@ -0,0 +1,26 @@
+﻿using APIFun.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace APIFun.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MarriottFoodController : ControllerBase
+    {
+        private IFoodRepository _foodRepository;
+
+        public MarriottFoodController(IFoodRepository temp)
+        {
+            _foodRepository = temp;
+        }
+
+        public IEnumerable<MarriottFood> Gett() 
+        {
+            var foodData = _foodRepository.Foods.ToArray();
+
+            return foodData;
+        }
+    }
+}
